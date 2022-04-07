6d8d32c4230bd5d54f45b653a2244134f4b0d311
Alex <acmitchell94@hotmail.com>
2022-04-07 12:19:06 +0100

    small header comment change

--- WordFrequencySorter.cpp
+++ WordFrequencySorter.cpp
@@ -3,25 +3,25 @@
 // DATE: 7th March 2022
 // VERSION: v1
 // DESCRIPTION: 
-// 
-// PART 1 - PRINTING WORDS IN ALPHABETICAL ORDER
-// 
-// This program reads in a text file of words and stores each word in a vector of strings.
-// This vector is used to construct a binary search tree (BST) of Treenodes.
-// Each Treenode in the BST contains a unique word from the text file with a counter of the number of times that unique word appeared in the text file (word
-// occurrence frequency).
-// The BST is printed in alphabetical order using in-order traversal.
-// 
-// PART 2 - PRINTING WORDS IN DESCENDING ORDER OF OCCURRENCE FREQUENCY:
+
+//PART 1 - PRINTING WORDS IN ALPHABETICAL ORDER
 //
-// In order to sort each Treenode in the BST in descending order of word occurrence frequency, a quicksort algorithm was used. 
-// This was accomplished as follows:
-// 
-// Whilst constructing the BST, the program generates a count of the number of Treenodes in the BST.
-// This Treenode count is used as an array size to create an array of Treenodes.
-// This array is then populated using in-order traversal of the BST, where each Treenode visited in the tree is placed in the array.
-// The Quick Sort algorithm is then applied to the array of Treenodes, in doing so ordering the Treenodes in descending order of word occurrence frequency.
-// Lastly, the array is printed descending from the last index to the first index, showing the words in descending order of their occurrence frequency.
+//This program reads in a text file of words and stores each word in a vector of strings.
+//This vector is used to construct a binary search tree (BST) of Treenodes.
+//Each Treenode in the BST contains a unique word from the text file with a counter of the number of times that unique word appeared in the text file (word
+//occurrence frequency).
+//The BST is printed in alphabetical order using in-order traversal.
+//
+//PART 2 - PRINTING WORDS IN DESCENDING ORDER OF OCCURRENCE FREQUENCY:
+//
+//In order to sort each Treenode in the BST in descending order of word occurrence frequency, a quicksort algorithm was used. 
+//This was accomplished as follows:
+//
+//Whilst constructing the BST, the program generates a count of the number of Treenodes in the BST.
+//This Treenode count is used as an array size to create an array of Treenodes.
+//This array is then populated using in-order traversal of the BST, where each Treenode visited in the tree is placed in the array.
+//The Quick Sort algorithm is then applied to the array of Treenodes, in doing so ordering the Treenodes in order of word occurrence frequency.
+//Lastly, the array is printed descending from the last index to the first index, showing the words in descending order of their occurrence frequency.
 
 #include<fstream>
 #include<string>
